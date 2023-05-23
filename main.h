e2d0c77383b49ea4939b8ff1bf02e29f01fe8b2f
move7 <addouni.fatima@gmail.com>
2023-05-24 01:47:28 +0200

    shell_cd function

--- main.h
+++ main.h
@@ -18,10 +18,14 @@ extern char **environ;
 #define ERR_MALLOC 1
 #define ERR_FORK 2
 #define ERR_PATH 3
+#define ERR_HOME 4
+#define ERR_OLD_PWD 5
 
 #define ERR_MALLOC_MESSAGE "Unable to malloc space\n"
 #define ERR_FORK_MESSAGE  "Unable to fork and create child process\n"
 #define ERR_PATH_MESSAGE  "No such file or directory\n"
+#define ERR_HOME_MESSAGE "HOME environment variable is not set\n"
+#define ERR_OLD_PWD_MESSAGE "OLD environment variable is not set\n" 
 /**
  * struct built_in_command - linked list of built-in commands
  * @name: name of command
@@ -31,13 +35,13 @@ extern char **environ;
  */
 typedef struct {
     char *name;
-    int (*func)(void);
+    int (*func)(char **args);
 } built_in_command;
 
 #define BUILT_IN_COMMANDS \
     { "cd", shell_cd }, \
     { "exit", shell_exit }, \
-    { "env", shell_env }, \
+    { "env", shell_env}, \
     { NULL, NULL }
 
 
@@ -58,9 +62,9 @@ char *read_line(void);
 char **get_arguments(char *str);
 
 /*-----------------------------built_in_commands.c -----*/
-int shell_cd(void);
-int shell_exit(void);
-int shell_env(void);
+int shell_cd(char** dir);
+int shell_exit(char __attribute__((__unused__)) **var);
+int shell_env(char __attribute__((__unused__)) **var);
 
 /*-----------------------------handl_exec.c -----*/
 int shell_execute(char **args);
@@ -70,5 +74,9 @@ char *get_path_variable();
 char *_find_cmd_path(char *path, char *cmd);
 char **duplicate_environ();
 
+/*-----------------------------handle_errors -----*/
 void _perror(int error);
+
+/*-----------------------------get_env -----*/
+char *_getenv(char *var);
 #endif
