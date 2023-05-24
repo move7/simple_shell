9a5f533eec6a9b38e7a23cf84b3ce015bb079ba2
Sarataxa <haimersara@gmail.com>
2023-05-24 03:12:52 -0700

    Correction

--- built_in_commands.c
+++ built_in_commands.c
@@ -4,17 +4,18 @@
 **shell_cd - changes the directory
 **Return: 1
 **/
-int shell_cd(void) {
-    
-    return 1;
+int shell_cd(void)
+{
+	return (1);
 }
 
 /**
 **shell_exit - exits the shell
 **Return: 0
 **/
-int shell_exit(void) {
-    return (-1);
+int shell_exit(void)
+{
+	return (-1);
 }
 
 /**
