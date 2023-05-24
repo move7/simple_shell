2b3f5912d081acbd725e125666d25a02b5b36789
Sarataxa <haimersara@gmail.com>
2023-05-24 08:47:38 -0700

    main.c

--- main.c
+++ main.c
@@ -1,5 +1,12 @@
 #include "main.h"
 
+/**
+ * main - entry point
+ * @ac: arg count
+ * @av: arg vector
+ *
+ * Return: 0 on success, 1 on error
+ */
 int main(int ac, char **av)
 {
 	char *input, **tokens;
