ef15016c85497c3428355b9674e6a1446e32b6c6
fatima <addouni.fatima@gmail.com>
2023-05-24 06:28:43 -0700

    betty syntax

--- handle_exec.c
+++ handle_exec.c
@@ -2,18 +2,13 @@
 
 int shell_execute(char **args)
 {
-    int i = 0;
-    built_in_command built_in_commands[] = { BUILT_IN_COMMANDS };
+	int i = 0;
+	built_in_command built_in_commands[] = { BUILT_IN_COMMANDS };
 
-    if (args[0] == NULL)
+	if (args[0] == NULL)
 		return (1);
-    
-    for (i = 0; built_in_commands[i].name != NULL; i++) 
-        if (_strcmp(args[0], built_in_commands[i].name) == 1) 
-            return built_in_commands[i].func(args+1);
-
-    
-    
-    return (1);
-    
+	for (i = 0; built_in_commands[i].name != NULL; i++)
+		if (_strcmp(args[0], built_in_commands[i].name) == 1)
+			return (built_in_commands[i].func(args + 1));
+	return (1);
 }
