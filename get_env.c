f6c0f7b1da35c63937cbf759c895c86d10c062f9
Sarataxa <haimersara@gmail.com>
2023-05-24 08:38:25 -0700

    get_env.c

--- get_env.c
+++ get_env.c
@@ -1,18 +1,11 @@
 #include "main.h"
 
 /**
-<<<<<<< HEAD
  * _getenv - return PATH
  * @var: environment variable to get
  * Return:  PATH environment variable
  */
 
-=======
- * get_path_variable - return PATH
- *
- * Return:  PATH environment variable
- */
->>>>>>> a55a477274a38fe51e2fb1670b5d959c0e1bd81b
 char *_getenv(char *var)
 {
 	int j, length, i = 0;
@@ -21,33 +14,23 @@ char *_getenv(char *var)
 
 	var = _concat_strings(var, "=", "");
 	length = _strlen(var);
-<<<<<<< HEAD
-=======
 
->>>>>>> a55a477274a38fe51e2fb1670b5d959c0e1bd81b
 	while (environ_copy[i] != NULL)
 	{
 		if (_strncmp(environ_copy[i], var, length) == 1)
 		{
-<<<<<<< HEAD
-		path_var = environ_copy[i] + length;	
-		path = malloc(_strlen(path_var) + 1);
+			path_var = environ_copy[i] + length;	
+			path = malloc(_strlen(path_var) + 1);
 			if (path == NULL) 
-=======
 			path_var = environ_copy[i] + length;
 			path = malloc(_strlen(path_var) + 1);
 			if (path == NULL)
->>>>>>> a55a477274a38fe51e2fb1670b5d959c0e1bd81b
 			{
 				_perror(ERR_MALLOC);
 				return (NULL);
 			}
 			path = _strcpy(path, path_var);
-<<<<<<< HEAD
 			for (j = 0; environ_copy[j] != NULL; j++) 
-=======
-			for (j = 0; environ_copy[j] != NULL; j++)
->>>>>>> a55a477274a38fe51e2fb1670b5d959c0e1bd81b
 				free(environ_copy[j]);
 			return (path);
 		}
